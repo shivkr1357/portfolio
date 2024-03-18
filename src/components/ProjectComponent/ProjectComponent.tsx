dc61501df8fba1466563fecc47b880a8cf579778
Abhishek Sharma <abhishek89079@gmail.com>
2024-03-18 18:10:07 +0530

    Changes

---
+++ src/components/ProjectComponent/ProjectComponent.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+const ProjectComponent = () => {
+  return (
+    <Stack>
+      <Image />
+    </Stack>
+  );
+};
+
+export default ProjectComponent;
--- src/components/WhatIKnowCard/WhatIKnow.tsx
+++ src/components/WhatIKnowCard/WhatIKnow.tsx
@@ -1,139 +1,144 @@
 "use client";
 import {
-   BACKEND_ITEMS,
-   DATABASE_ITEMS,
-   DEVOPS_ITEMS,
-   FRONETND_ITEMS,
+  BACKEND_ITEMS,
+  DATABASE_ITEMS,
+  DEVOPS_ITEMS,
+  FRONETND_ITEMS,
 } from "@/utils/constant";
 import { Stack, Typography, styled } from "@mui/material";
 import React from "react";
+import ProjectComponent from "../ProjectComponent/ProjectComponent";
 
 const CustomTypography = styled(Typography, {
-   name: "Typography",
-   slot: "root",
+  name: "Typography",
+  slot: "root",
 })(({ theme }) => ({
-   backgroundColor: "rgb(20,19,25)",
-   display: "inline-block",
-   justifyContent: "center",
-   alignItems: "center",
-   padding: "8px",
-   margin: "5px",
-   fontSize: "11px",
-   fontWeight: 500,
-   borderRadius: "5px",
-   color: "gray",
+  backgroundColor: "rgb(20,19,25)",
+  display: "inline-block",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "8px",
+  margin: "5px",
+  fontSize: "11px",
+  fontWeight: 500,
+  borderRadius: "5px",
+  color: "gray",
 }));
 
 const CustomStack = styled(Stack, { name: "Stack", slot: "root" })(
-   ({ theme }) => ({
-      backgroundColor: "rgb(27,26,33)",
-      padding: "10px",
-      borderRadius: "10px",
-      gap: 2,
-      margin: "10px",
-      [theme.breakpoints.up("xs")]: {
-         width: "90%",
-      },
-      [theme.breakpoints.up("sm")]: {
-         width: "45%",
-         height: "50%",
-      },
-      [theme.breakpoints.up("md")]: {
-         width: "14%",
-         height: "94%",
-      },
-   })
+  ({ theme }) => ({
+    backgroundColor: "rgb(27,26,33)",
+    padding: "10px",
+    borderRadius: "10px",
+    gap: 2,
+    margin: "10px",
+    [theme.breakpoints.up("xs")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "45%",
+      height: "50%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "14%",
+      height: "94%",
+    },
+  })
 );
 
 const WhatIKnow = () => {
-   return (
-      <Stack gap={2}>
-         <Typography marginLeft={2} marginTop={5}>
-            What I know
-         </Typography>
-         <Stack
-            direction={"row"}
-            style={{
-               display: "flex",
-               flexWrap: "wrap",
-               alignItems: "center",
-               marginLeft: "20px",
-            }}
-         >
-            <CustomStack>
-               <Typography>Frontend</Typography>
-               <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
-                  {FRONETND_ITEMS.map((item, index) => {
-                     return (
-                        <CustomTypography
-                           style={{ border: `1px solid ${item.color} ` }}
-                           key={item.name + item.color}
-                        >
-                           {item.name}
-                        </CustomTypography>
-                     );
-                  })}
-               </Stack>
-            </CustomStack>
-            <CustomStack>
-               <Typography>Backend</Typography>
-               <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
-                  {BACKEND_ITEMS.map((item, index) => {
-                     return (
-                        <CustomTypography
-                           style={{ border: `1px solid ${item.color} ` }}
-                           key={item.name + item.color}
-                        >
-                           {item.name}
-                        </CustomTypography>
-                     );
-                  })}
-               </Stack>
-            </CustomStack>
-            <CustomStack>
-               <Typography>Databases</Typography>
-               <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
-                  {DATABASE_ITEMS.map((item, index) => {
-                     return (
-                        <CustomTypography
-                           style={{ border: `1px solid ${item.color} ` }}
-                           key={item.name + item.color}
-                        >
-                           {item.name}
-                        </CustomTypography>
-                     );
-                  })}
-               </Stack>
-            </CustomStack>
-            <CustomStack>
-               <Typography>DevOps</Typography>
-               <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
-                  {DEVOPS_ITEMS.map((item, index) => {
-                     return (
-                        <CustomTypography
-                           style={{ border: `1px solid ${item.color} ` }}
-                           key={item.name + item.color}
-                        >
-                           {item.name}
-                        </CustomTypography>
-                     );
-                  })}
-               </Stack>
-            </CustomStack>
+  return (
+    <Stack gap={4} marginLeft={10}>
+      <Typography marginLeft={2} marginTop={5}>
+        What I know
+      </Typography>
+      <Stack
+        direction={"row"}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        <CustomStack>
+          <Typography>Frontend</Typography>
+          <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
+            {FRONETND_ITEMS.map((item, index) => {
+              return (
+                <CustomTypography
+                  style={{ border: `1px solid ${item.color} ` }}
+                  key={item.name + item.color}
+                >
+                  {item.name}
+                </CustomTypography>
+              );
+            })}
+          </Stack>
+        </CustomStack>
+        <CustomStack>
+          <Typography>Backend</Typography>
+          <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
+            {BACKEND_ITEMS.map((item, index) => {
+              return (
+                <CustomTypography
+                  style={{ border: `1px solid ${item.color} ` }}
+                  key={item.name + item.color}
+                >
+                  {item.name}
+                </CustomTypography>
+              );
+            })}
+          </Stack>
+        </CustomStack>
+        <CustomStack>
+          <Typography>Databases</Typography>
+          <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
+            {DATABASE_ITEMS.map((item, index) => {
+              return (
+                <CustomTypography
+                  style={{ border: `1px solid ${item.color} ` }}
+                  key={item.name + item.color}
+                >
+                  {item.name}
+                </CustomTypography>
+              );
+            })}
+          </Stack>
+        </CustomStack>
+        <CustomStack>
+          <Typography>DevOps</Typography>
+          <Stack sx={{ display: "inline-block", marginTop: "20px" }}>
+            {DEVOPS_ITEMS.map((item, index) => {
+              return (
+                <CustomTypography
+                  style={{ border: `1px solid ${item.color} ` }}
+                  key={item.name + item.color}
+                >
+                  {item.name}
+                </CustomTypography>
+              );
+            })}
+          </Stack>
+        </CustomStack>
 
-            <Stack
-               sx={{
-                  backgroundColor: "rgb(27,26,33)",
-                  padding: "10px",
-                  gap: 2,
-                  margin: "10px",
-                  width: { xs: "90%", sm: "45%", md: "24%" },
-                  height: { xs: "100%", sm: "100%" },
-               }}
-            ></Stack>
-         </Stack>
+        <Stack
+          sx={{
+            backgroundColor: "rgb(27,26,33)",
+            padding: "10px",
+            gap: 2,
+            margin: "10px",
+            width: { xs: "90%", sm: "45%", md: "24%" },
+            height: { xs: "100%", sm: "100%" },
+          }}
+        >
+          <Typography sx={{ fontSize: "18p" }}>More Projects</Typography>
+          <Stack>
+            <ProjectComponent />
+          </Stack>
+        </Stack>
       </Stack>
-   );
+    </Stack>
+  );
 };
 
 export default WhatIKnow;
